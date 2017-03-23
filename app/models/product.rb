69bde442f69aff88f5465fbe4a7454127d9cfa23
CorinneKelly <corinnemariekelly@gmail.com>
2017-03-23 17:53:58 -0400

    Done.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,22 @@
 class Product < ActiveRecord::Base
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
+
+    def self.serialize(product)
+ 
+    # start with the open brace to create a valid JSON object
+    serialized_product = '{'
+ 
+    serialized_product += '"name": "' + product.name + '", '
+    serialized_product += '"price": "' + product.price.to_s + '", '
+    serialized_product += '"inventory": ' + product.inventory.to_s + ', '
+    serialized_product += '"description": "' + product.description + '", '
+ 
+    # the author association can also be represented in JSON
+    serialized_product += '"orders": {'
+    serialized_product += '"name": "' + product.orders.name + '"}'
+ 
+    # and end with the close brace
+    serialized_product += '}'
+  end
 end
